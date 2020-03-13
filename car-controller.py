57e9dc3ecac8c26c4ba0b8604d986ead0312b4c5
RoozbehBandpey <r.bandpey67@gmail.com>
2020-03-13 16:54:58 +0100

    capture image from front camera

--- car-controller.py
+++ car-controller.py
@@ -19,14 +19,30 @@ except IndexError as err:
 import carla
 import random
 import time
+import numpy as np
+import cv2
+
 
 actors = []
 IM_WIDTH = 640
-IM_HIGHT = 480
+IM_HEIGHT = 480
 
 
 def process_img(image):
-	
+	i = np.array(image.raw_data)
+	print(i.shape)
+	i2 = i.reshape((IM_HEIGHT, IM_WIDTH, 4))
+
+	# The entire height, the entire width, thr first three of r, g, b, a
+	# Kinda nifty way to grab r,g,b there is an open cv method to do the exact same thing
+	# Basically converts rgb alpha to just straight up  rgb, interestingly enough it is slower than the numpy method
+	# You would have expected it to do the same thing, but simply does not
+	i3 = i2[:, :, :3]
+	print(i3.shape)
+	cv2.imshow("", i3)
+	cv2.waitKey(1)
+	return i3/255.0
+
 
 # -- attempt to connect to server and clean ups
 try:
@@ -49,16 +65,16 @@ try:
 	# attach a camera sensor to the car
 	cam_blueprint = blueprint_library.find("sensor.camera.rgb")
 	cam_blueprint.set_attribute("image_size_x", f"{IM_WIDTH}")
-	cam_blueprint.set_attribute("image_size_y", f"{IM_HIGHT}")
+	cam_blueprint.set_attribute("image_size_y", f"{IM_HEIGHT}")
 	cam_blueprint.set_attribute("fov", "110")
 
 
-	spawn_point = carla.Trasform(carla.Location(x=2.5, z=0.7))
+	spawn_point = carla.Transform(carla.Location(x=2.5, z=0.7))
 	sensor = world.spawn_actor(cam_blueprint, spawn_point, attach_to=vehicle)
 	actors.append(sensor)
 
 	# listening
-	sensor.listen(lambda dsta: process_img(data))
+	sensor.listen(lambda data: process_img(data))
 
 	time.sleep(15)
 
