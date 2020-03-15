1bac75f1541e26e2b2814fcb7f7d76d185105217
RoozbehBandpey <r.bandpey67@gmail.com>
2020-03-15 15:58:52 +0100

    cleaned up dqn

--- dqn-lib/dqn.py
+++ dqn-lib/dqn.py
@@ -2,22 +2,12 @@
 
 
 
-class DQN(object):
+class DQN:
 	def __init__(self):
 		pass
 
-	def step(self, action):
-		"""
-		param: action
-		:return next observation
-		:return rward
-		:return: done boolean flag wheather or not we're done
-		:return: extra info
-		"""
-		return obs, reward, done, _
 
 
-	def reset(self):
-		"""
-		run annother episode
-		"""
+
+if __name__ == "__main__":
+	pass
